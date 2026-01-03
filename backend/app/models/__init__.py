e96ca5881e8033a8b29f1cb9329e32582cbfd172
darunesh1 <sathyarajasekar5873@gmail.com>
2026-01-03 11:42:57 +0530

    added the city and stop the package

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,7 @@
 from app.database import Base
+from app.models.city import City
+from app.models.stop import Stop
 from app.models.trip import Trip
 from app.models.user import User
 
-__all__ = ["User", "Trip", "Base"]
+__all__ = ["User", "Trip", "City", "Stop", "Base"]
