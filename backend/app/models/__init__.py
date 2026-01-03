c019bdc9b672462d0accca6127a1c6d10e3a131d
darunesh1 <sathyarajasekar5873@gmail.com>
2026-01-03 12:57:21 +0530

    updated the package init of models with Activity and StopActivity

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,8 @@
 from app.database import Base
+from app.models.activity import Activity, StopActivity
 from app.models.city import City
 from app.models.stop import Stop
 from app.models.trip import Trip
 from app.models.user import User
 
-__all__ = ["User", "Trip", "Stop", "City", "Base"]
+__all__ = ["User", "Trip", "Stop", "City", "Activity", "StopActivity", "Base"]
